9c9d7101b8eca78377f3a744438672266b252acf
Richard OLVERA <richard.olvera@orange.com>
2024-11-21 11:32:07 +0100

    init generator openapi

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.openapitools.api.DefaultApi;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/troubleTicket")
+public class TestController implements DefaultApi {}
